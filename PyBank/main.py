bdf2f595ef3e11ba6f8bc773e52974209924fc1e
Alex Riner <rineralexander@gmail.com>
2020-08-17 15:25:09 -0500

    implimented total months, total profit and average change in pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,27 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 content=[]
+totalMonths=0
+totalProfit=0
+grtIncrease=0
+grtDecrease=0
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     for row in csvreader:
         content.append(row)
-print(content)
+for row in content:
+    totalMonths=totalMonths+1
+    profit=int(row[1])
+    totalProfit=totalProfit+profit
+    if profit>grtIncrease:
+        grtIncrease = profit
+    elif profit<grtDecrease:
+        grtDecrease = profit
+averageChange=totalProfit/totalMonths
+output=["Financial Analysis","----------------------------","Total Months: "+str(totalMonths),
+"Total: "+str(totalProfit),"Average  Change: "+str(averageChange)]
+for line in output:
+    print(line)
+    
