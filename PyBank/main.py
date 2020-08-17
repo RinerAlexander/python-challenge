4e2acc5e24fbf22e13bde9abe39dc446efec5155
Alex Riner <rineralexander@gmail.com>
2020-08-17 15:38:27 -0500

    Added Greatest Increase and Greatest Decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,11 +2,14 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
 content=[]
 totalMonths=0
 totalProfit=0
 grtIncrease=0
+grtInMonth="No Increase in Profits"
 grtDecrease=0
+grtDeMonth="No Decrease in Profits"
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -19,11 +22,16 @@ for row in content:
     totalProfit=totalProfit+profit
     if profit>grtIncrease:
         grtIncrease = profit
+        grtInMonth = row[0]
     elif profit<grtDecrease:
         grtDecrease = profit
+        grtDeMonth = row[0]
 averageChange=totalProfit/totalMonths
 output=["Financial Analysis","----------------------------","Total Months: "+str(totalMonths),
-"Total: "+str(totalProfit),"Average  Change: "+str(averageChange)]
+"Total: "+str(totalProfit),"Average  Change: "+str(averageChange),
+f"Greatest Increase in Profits: {grtInMonth} ({grtIncrease})",
+f"Greatest Decrease in Profits: {grtDeMonth} ({grtDecrease})"]
 for line in output:
     print(line)
+    
     
