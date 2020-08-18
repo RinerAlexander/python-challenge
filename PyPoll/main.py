aba03b18da31232593f2448d2acf44562b2701d0
Alex Riner <rineralexander@gmail.com>
2020-08-18 10:09:00 -0500

    Not(None) returns true, which is useful

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,8 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 outputpath = os.path.join('analysis', 'output.txt')
 content=[]
 voteDict = {}
+winner=None
+total = 0
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -14,8 +16,14 @@ with open(csvpath, 'r') as csvfile:
 
 for row in content:
     candidate=row[2]
+    total=total+1
     if candidate in voteDict:
         voteDict[candidate]=voteDict[candidate]+1
     else:
         voteDict[candidate]=1
-print(voteDict)
+
+for candidate in voteDict:
+    if not(winner):
+        winner=candidate
+print(winner)
+
