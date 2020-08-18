aac53d62d7034fd3fd1ef28dd1bd07fe01be2b5f
Alex Riner <rineralexander@gmail.com>
2020-08-18 09:47:20 -0500

    vote counter not quite working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,10 +4,18 @@ import os
 csvpath = os.path.join('Resources', 'election_data.csv')
 outputpath = os.path.join('analysis', 'output.txt')
 content=[]
+voteDict = {}
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     for row in csvreader:
         content.append(row)
-print(content[2])
+
+for row in content:
+    candidate=row[2]
+    if candidate in voteDict:
+        voteDict={candidate:voteDict[candidate]+1}
+    else:
+        voteDict={candidate:1}
+print(voteDict)
